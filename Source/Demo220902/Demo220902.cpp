6fdb00d9bb035f75dafd2573c7aa53ef20f2e99f
CooCoChoco_索 <35162651+suozhiyuan@users.noreply.github.com>
2022-10-21 23:56:55 +0800

    Delete Demo220902.cpp

--- Source/Demo220902/Demo220902.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Demo220902.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Demo220902, "Demo220902" );
