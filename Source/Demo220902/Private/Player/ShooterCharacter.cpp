57d321d4404180f17833f797f2ed496b5bdd325b
suozhiyuan <290559325@qq.com>
2022-10-24 21:50:18 +0800

    修改格式

--- Source/Demo220902/Private/Player/ShooterCharacter.cpp
+++ Source/Demo220902/Private/Player/ShooterCharacter.cpp
@@ -100,11 +100,8 @@ void AShooterCharacter::Tick(float DeltaTime)
 void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-	// TEXT 中是编辑器中设置的输入轴的名字
 	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AShooterCharacter::MoveForward);
 	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AShooterCharacter::MoveRight);
-
 	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &AShooterCharacter::AddControllerYawInput);			// Yaw 左右
 	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AShooterCharacter::AddControllerPitchInput);		// Pitch 上下
 }
