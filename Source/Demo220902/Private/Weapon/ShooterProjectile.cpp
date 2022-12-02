eee68948c87fcbe3ae81949915bbc23fe482dee7
suozhiyuan <290559325@qq.com>
2022-12-02 11:53:20 +0800

    子弹移动组件的实现

--- Source/Demo220902/Private/Weapon/ShooterProjectile.cpp
+++ Source/Demo220902/Private/Weapon/ShooterProjectile.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Demo220902.h"
 #include "Weapon/ShooterProjectile.h"
+#include "Demo220902.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 
@@ -12,7 +12,7 @@ AShooterProjectile::AShooterProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// 碰撞组件的设置
+	// 碰撞组件的初始化
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));			// 创建组件或子对象，模板类型为创建类型，参数为组件名称
 	CollisionComp->InitSphereRadius(5.0f);													// 在不触发渲染或物理更新的情况下设置的球体半径
 	CollisionComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);					// 设置碰撞类型，为减少运算量不做物理模拟
@@ -23,8 +23,13 @@ AShooterProjectile::AShooterProjectile()
 	CollisionComp->SetCollisionResponseToChannel(ECC_Pawn,ECR_Block);				// 对Pawn产生碰撞
 	RootComponent = CollisionComp;		// 设置为根组件
 
+	// 一个专门用于投射物体的移动组件初始化
 	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
-
+	MovementComp->UpdatedComponent = CollisionComp;						// 控制哪个Component
+	MovementComp->InitialSpeed = 2000.0f;								// 初始化速度
+	MovementComp->MaxSpeed = 2000.0f;									// 最大速度
+	MovementComp->bRotationFollowsVelocity = true;						// 是否旋转
+	MovementComp->ProjectileGravityScale = 0.0f;						// 重量，影响下坠
 }
 
 // Called when the game starts or when spawned
@@ -43,6 +48,7 @@ void AShooterProjectile::Tick(float DeltaTime)
 
 void AShooterProjectile::InitVelocity(FVector ShooterDirection)
 {
-	// 
+	// Velocity: 更新组件的当前速度
+	MovementComp->Velocity = ShooterDirection * MovementComp->InitialSpeed;
 }
 
--- Source/Demo220902/Public/Weapon/ShooterProjectile.h
+++ Source/Demo220902/Public/Weapon/ShooterProjectile.h
@@ -22,6 +22,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -36,5 +38,4 @@ protected:
 	// 一个专门用于投射物体的移动组件
 	UPROPERTY(EditDefaultsOnly, Category = Projectile)
 	UProjectileMovementComponent* MovementComp;
-
 };
