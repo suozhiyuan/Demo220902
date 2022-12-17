d2c4804477529c0a837120aa6f464a1b46a9028b
suozhiyuan <290559325@qq.com>
2022-12-17 23:50:58 +0800

    优化代码

--- Source/Demo220902/Private/Weapon/ShooterWeapon_Projectile.cpp
+++ Source/Demo220902/Private/Weapon/ShooterWeapon_Projectile.cpp
@@ -17,7 +17,7 @@ void AShooterWeapon_Projectile::FireWeapon()
 
 	const FVector ShooterDirection = GetAdjustAim();							// 眼睛方向
 	const FVector Origin = GetMuzzleLocation();									// 枪口位置
-	const FTransform SpawnTM(ShooterDirection.Rotation(), Origin);		// 通过参数的引用获取对象的位置和方向
+	const FTransform SpawnTM(ShooterDirection.Rotation(), Origin);				// 通过参数的引用获取对象的位置和方向
 
 	 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	  * BeginDeferredActorSpawnFromClass：为了让参数全部初始化完毕，延迟生成参与者的实例
@@ -28,10 +28,10 @@ void AShooterWeapon_Projectile::FireWeapon()
 	AShooterProjectile* Projectile = Cast<AShooterProjectile>(UGameplayStatics::BeginDeferredActorSpawnFromClass(this, ProjectileConfig.ProjectileClass, SpawnTM));
 	if (Projectile)
 	{
-		Projectile->SetInstigator(GetInstigator());						// 保持统一的Instigator
-		Projectile->SetOwner(this);										// 设置子弹拥有者，在计算子弹伤害时会从拥有者身上取属性
-		Projectile->InitVelocity(ShooterDirection);						// 设置子弹速度，传进去的是方向角度，和速度有啥关系
-		UGameplayStatics::FinishSpawningActor(Projectile, SpawnTM);		// 对应BeginDeferredActorSpawnFromClass，延迟结束
+		Projectile->SetInstigator(GetInstigator());								// 保持统一的Instigator
+		Projectile->SetOwner(this);												// 设置子弹拥有者，在计算子弹伤害时会从拥有者身上取属性
+		Projectile->InitVelocity(ShooterDirection);								// 设置子弹速度，传进去的是方向角度，和速度有啥关系
+		UGameplayStatics::FinishSpawningActor(Projectile, SpawnTM);				// 对应BeginDeferredActorSpawnFromClass，延迟结束
 	}
 }
 
