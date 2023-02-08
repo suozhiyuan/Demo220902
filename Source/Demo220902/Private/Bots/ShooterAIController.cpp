c2131d20a216e48e8c933836cbb6466f71bcd004
suozhiyuan <290559325@qq.com>
2023-02-08 09:09:56 +0800

    修改代码

--- Source/Demo220902/Private/Bots/ShooterAIController.cpp
+++ Source/Demo220902/Private/Bots/ShooterAIController.cpp
@@ -2,4 +2,51 @@
 
 
 #include "Bots/ShooterAIController.h"
+#include "Player/ShooterCharacter.h"
 
+
+// 找到可见的，并且是最近的敌人  （LOS：Line of Sight 视线）
+void AShooterAIController::FindRecentEnemyLOS(AShooterCharacter* ExcludeEnemy)
+{
+	if (APawn* MyBot = GetPawn())
+	{
+		float BestDistSq = MAX_FLT;								// 记录遍历到的最近距离
+		const FVector MyLoc = MyBot->GetActorLocation();		// 获取当前角色位置
+		AShooterCharacter* BestPawn = nullptr;					// 保存距离最近的目标
+
+		/*
+		 * 模仿迭代器类，当TArray<TWeakObjectPtr<APawn>>::Iterator 被定义时，试图提供FConstPawnIterator之前所做的基本接口，
+		 * 一般来说，你不应该使用这个迭代器，而应该使用TActorIterator<APawn>,或TActorRange<APawn> (或所需的派生类型)。在未来的版本中，这个迭代器可能会被弃用。
+		 */
+		for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; It++)
+		{
+			AShooterCharacter* TestPawn = Cast<AShooterCharacter>(*It);			// 遍历获取一个目标
+			if (TestPawn && TestPawn != ExcludeEnemy)
+			{
+				if (HasLOSToEnemy(TestPawn, true))						// 查找视线内的敌人
+				{
+					const float DistSq = (TestPawn->GetActorLocation() - MyLoc).SizeSquared();		// SizeSquared 向量长度的平方
+					if (DistSq < BestDistSq)
+					{
+						BestDistSq = DistSq;
+						BestPawn = TestPawn;
+					}
+				}
+			}
+		}
+		// 记录到黑板数据
+		if (BestPawn)
+		{
+			
+		}
+
+	}
+}
+
+// 视线方向是否有敌人 （LOS：Line of Sight 视线）
+bool AShooterAIController::HasLOSToEnemy(AActor* InEnemyActor, bool bAnyEnemy) const
+{
+	// LineTraceSingleByChannel 从机器人位置，发射特定的通道跟踪光线，并返回第一个阻塞命中
+	GetWorld()->LineTraceSingleByChannel();
+	return true;
+}
--- Source/Demo220902/Public/Bots/ShooterAIController.h
+++ Source/Demo220902/Public/Bots/ShooterAIController.h
@@ -16,8 +16,14 @@ class DEMO220902_API AShooterAIController : public AAIController
 	GENERATED_BODY()
 
 public:
-	// 找到可见的，最近的敌人  （LOS：Line of Sight）
+	// 找到可见的，并且是最近的敌人  （LOS：Line of Sight 视线）
+	UFUNCTION(BlueprintCallable, Category = Behavior)
 	void FindRecentEnemyLOS(AShooterCharacter* ExcludeEnemy);
+
+	// 视线方向是否有敌人 （LOS：Line of Sight 视线）
+	bool HasLOSToEnemy(AActor* InEnemyActor, bool bAnyEnemy) const;
+
+
 
 
 	
